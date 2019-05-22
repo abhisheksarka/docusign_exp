26c1366b50ea730236f77cf7489d3fefceaffbd1
Abhishek Sarkar <abhisheksarka@gmail.com>
2019-05-22 15:10:21 +0530

    init commit

--- app/controllers/embedded_signing_controller.rb
+++ app/controllers/embedded_signing_controller.rb
@@ -1,81 +1,64 @@
 class EmbeddedSigningController < ApplicationController
-  # This is a quick start example of embedding the signing ceremony within your website.
-  # Language: Ruby with Ruby on Rails
-  #
-  # See the Readme and Setup files for more information.
-  #
-  # Copyright (c) DocuSign, Inc.
-  # License: MIT Licence. See the LICENSE file.
-  #
-  # This example does not include authentication. Instead, an access token
-  # must be supplied from the Token Generator tool on the DevCenter or from
-  # elsewhere.
-  #
-  # This example also does not look up the DocuSign account id to be used.
-  # Instead, the account id must be set.
-  #
-  # For a more production oriented example, see the
-  # Authorization code grant authentication example:
-  #      https://github.com/docusign/eg-03-ruby-auth-code-grant
-  def create
-    # base_url is the url of this application. Eg http://localhost:3000
-    base_url = request.base_url
 
-    # Fill in these constants
-    # Obtain an OAuth token from https://developers.hqtest.tst/oauth-token-generator
-    access_token = '{ACCESS_TOKEN}'
-
-    # Obtain your accountId from demo.docusign.com -- the account id is shown in the drop down on the
-    # upper right corner of the screen by your picture or the default picture.
-    account_id = '{ACCOUNT_ID}'
-
-    # Recipient Information:
-    signer_name = '{USER_FULLNAME}'
-    signer_email = '{USER_EMAIL}'
-
-    base_path = 'http://demo.docusign.net/restapi'
-    client_user_id = '123' # Used to indicate that the signer will use an embedded
-    # Signing Ceremony. Represents the signer's userId within
-    # your application.
-    authentication_method = 'None' # How is this application authenticating
-    # the signer? See the `authenticationMethod' definition
-    file_name = 'World_Wide_Corp_lorem.pdf' # The document to be signed.
-
-    # Step 1. Create the envelope request definition
+  def build_envelope(pdf_filename)
     envelope_definition = DocuSign_eSign::EnvelopeDefinition.new
-    envelope_definition.email_subject = "Please sign this document sent via the Ruby SDK"
+    envelope_definition.email_subject = 'Please sign this document sent from Ruby SDK'
+    doc = DocuSign_eSign::Document.new
 
-    doc = DocuSign_eSign::Document.new({
-      :documentBase64 => Base64.encode64(File.binread(File.join('data', file_name))),
-      :name => "Lorem Ipsum", :fileExtension => "pdf", :documentId => "1"})
+    doc.document_base64 = Base64.encode64(File.binread(File.join('data', pdf_filename)))
+    doc.name = 'Lorem Ipsum'
+    doc.file_extension = 'pdf'
+    doc.document_id = '2'
 
-    # The order in the docs array determines the order in the envelope
     envelope_definition.documents = [doc]
-    # create a signer recipient to sign the document, identified by name and email
-    # We're setting the parameters via the object creation
-    signer = DocuSign_eSign::Signer.new ({
-        :email => signer_email, :name => signer_name,
-        :clientUserId => client_user_id,  :recipientId => 1
+    envelope_definition.status = 'sent'
+    envelope_definition
+  end
+
+  def build_signer(envelope_definition, signer_email, signer_name, signer_id)
+    signer = DocuSign_eSign::Signer.new({
+      email: signer_email,
+      name: signer_name,
+      clientUserId: signer_id,
+      recipientId: signer_id,
+      routingOrder: 0
     })
-    sign_here = DocuSign_eSign::SignHere.new ({
-        :documentId => '1', :pageNumber => '1',
-        :recipientId => '1', :tabLabel => 'SignHereTab',
-        :xPosition => '195', :yPosition => '147'
+    recipients = envelope_definition.recipients
+    if recipients.present?
+      recipients.signers << signer
+    else
+      recipients = DocuSign_eSign::Recipients.new
+      recipients.signers = [signer]
+      envelope_definition.recipients = recipients
+    end
+    signer
+  end
+
+  def build_sign(signer, x, y)
+    sign_here = DocuSign_eSign::SignHere.new({
+      documentId: '2',
+      pageNumber: '1',
+      recipentId: signer.client_user_id,
+      tabLabel: 'SignHere',
+      xPosition: '195',
+      yPosition: '147',
     })
+    signer.tabs = DocuSign_eSign::Tabs.new({signHereTabs: [sign_here]})
+    sign_here
+  end
 
-    # Tabs are set per recipient / signer
-    tabs = DocuSign_eSign::Tabs.new({:signHereTabs => [sign_here]})
-    signer.tabs = tabs
-    # Add the recipients to the envelope object
-    recipients = DocuSign_eSign::Recipients.new({:signers => [signer]})
+  def init_envelope
+    access_token = 'eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQoAAAABAAUABwCA7HpJcN7WSAgAgCyeV7Pe1kgCAKsZBs9RSxNPgv5x1_3YGSAVAAEAAAAYAAkAAAAFAAAAKwAAAC0AAAAvAAAAMQAAADIAAAA4AAAAMwAAADUAAAANACQAAABmMGYyN2YwZS04NTdkLTRhNzEtYTRkYS0zMmNlY2FlM2E5NzgSAAEAAAALAAAAaW50ZXJhY3RpdmUwAABW4khw3tZINwDJqZl5AmSpSpeEeFrVSdWh.uk6XpN_yB3IZ7EabT5B328zA2GhYKeuNQM50bmQOY2XylI6auXqz876zgYcplP5BnjTs6oGMabIn2SmnpzNMrF0D4nZ0XDGqvtUf_KNul209o-ZVqL5AeUPhKlXAZP2NPVwf-02UDq3I95fmjW_BYg8I5kwlRHgSLSzW7eGKr3iUal7Uf3q2Eq2vBRPI1XrKbjT7SoabGJ_mbequvgcR7d8Y8KWClRVk2TnULY18blPjSakqqjsI_u0KyvK62m4pmLSrAXiO1rKk0Rmhs4Px4e1YxGR36xD9L_3zT_dtVKZ4FV1V6fOVV_aaa8AR1dJMIQFidcHOaacvAMci8gOmnQ'
+    account_id = '8289969'
+    base_path = 'http://demo.docusign.net/restapi'
 
-    envelope_definition.recipients = recipients
-    # Request that the envelope be sent by setting |status| to "sent".
-    # To request that the envelope be created as a draft, set to "created"
-    envelope_definition.status = "sent"
+    envelope_definition = build_envelope("World_Wide_Corp_lorem.pdf")
+    signer1 = build_signer(envelope_definition, "abhishek@synup.com", "Abhishek", 1)
+    build_sign(signer1, 10, 10)
 
-    # Step 2. Call DocuSign with the envelope definition to have the
-    #         envelope created and sent
+    signer2 = build_signer(envelope_definition, "binod@synup.com", "Binod", 2)
+    build_sign(signer2, 30, 10)
+
     configuration = DocuSign_eSign::Configuration.new
     configuration.host = base_path
     api_client = DocuSign_eSign::ApiClient.new configuration
@@ -83,7 +66,25 @@ class EmbeddedSigningController < ApplicationController
     envelopes_api = DocuSign_eSign::EnvelopesApi.new api_client
 
     results = envelopes_api.create_envelope account_id, envelope_definition
-    envelope_id = results.envelope_id
+    results.envelope_id
+  end
+
+  def create
+    base_url = request.base_url
+    access_token = 'eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQoAAAABAAUABwCA7HpJcN7WSAgAgCyeV7Pe1kgCAKsZBs9RSxNPgv5x1_3YGSAVAAEAAAAYAAkAAAAFAAAAKwAAAC0AAAAvAAAAMQAAADIAAAA4AAAAMwAAADUAAAANACQAAABmMGYyN2YwZS04NTdkLTRhNzEtYTRkYS0zMmNlY2FlM2E5NzgSAAEAAAALAAAAaW50ZXJhY3RpdmUwAABW4khw3tZINwDJqZl5AmSpSpeEeFrVSdWh.uk6XpN_yB3IZ7EabT5B328zA2GhYKeuNQM50bmQOY2XylI6auXqz876zgYcplP5BnjTs6oGMabIn2SmnpzNMrF0D4nZ0XDGqvtUf_KNul209o-ZVqL5AeUPhKlXAZP2NPVwf-02UDq3I95fmjW_BYg8I5kwlRHgSLSzW7eGKr3iUal7Uf3q2Eq2vBRPI1XrKbjT7SoabGJ_mbequvgcR7d8Y8KWClRVk2TnULY18blPjSakqqjsI_u0KyvK62m4pmLSrAXiO1rKk0Rmhs4Px4e1YxGR36xD9L_3zT_dtVKZ4FV1V6fOVV_aaa8AR1dJMIQFidcHOaacvAMci8gOmnQ'
+    account_id = '8289969'
+
+    signer_name = 'Abhishek'
+    signer_email = 'abhishek@synup.com'
+    signer_id = 1
+
+    base_path = 'http://demo.docusign.net/restapi'
+
+    authentication_method = 'None'
+    configuration = DocuSign_eSign::Configuration.new
+    configuration.host = base_path
+    api_client = DocuSign_eSign::ApiClient.new configuration
+    api_client.default_headers["Authorization"] = "Bearer " + access_token
 
     # Step 3. create the recipient view request for the Signing Ceremony
     view_request =  DocuSign_eSign::RecipientViewRequest.new
@@ -98,10 +99,16 @@ class EmbeddedSigningController < ApplicationController
     # we used to create the envelope.
     view_request.email = signer_email
     view_request.user_name = signer_name
-    view_request.client_user_id = client_user_id
+    view_request.client_user_id = signer_id
+
+    configuration = DocuSign_eSign::Configuration.new
+    configuration.host = base_path
+    api_client = DocuSign_eSign::ApiClient.new configuration
+    api_client.default_headers["Authorization"] = "Bearer " + access_token
+    envelopes_api = DocuSign_eSign::EnvelopesApi.new api_client
 
     # Step 4. call the CreateRecipientView API
-    results = envelopes_api.create_recipient_view account_id, envelope_id, view_request
+    results = envelopes_api.create_recipient_view account_id, "6abeaa31-3c38-4a64-ab96-49853e6bb930", view_request
 
     # Step 5. Redirect the user to the Signing Ceremony
     # Don't use an iFrame!
